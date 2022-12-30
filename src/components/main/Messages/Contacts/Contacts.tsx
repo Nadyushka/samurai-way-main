226039ab8ea3b3257ed3116d48f7f5498a0df049
Nadyushka <12nadya11@gmail.com>
2022-12-30 19:03:32 +0300

    lesson 49 samurai-way start

--- src/components/main/Messages/Contacts/Contacts.tsx
+++ src/components/main/Messages/Contacts/Contacts.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import s from './contacts.module.css'
 import Contact from "./Contact/Contact";
+import {contactDataType} from "./ContactsContainer";
 
 
+type PropsType = {
+    contacts:contactDataType[]
+}
 
 
-
-const Contacts = () => {
+const Contacts = (props:PropsType) => {
 
     return (
         <div className={s.contacts}>
 
-            {/*{props.contacts.map(contact => <Contact key={contact.id} name={contact.name} id={contact.id}/>)} */}
+            {props.contacts.map(contact => <Contact key={contact.id} name={contact.name} id={contact.id}/>)}
 
         </div>
     );
