816ed1e1ca227a8e36fbafa4dd4459c45d30264d
Nadyushka <12nadya11@gmail.com>
2022-12-31 19:12:28 +0300

    lesson 49 samurai-way fill the UsersContainer

--- src/components/main/users/UsersContainer.tsx
+++ src/components/main/users/UsersContainer.tsx
@@ -1,14 +1,32 @@
 import {connect} from "react-redux";
 import Posts from "../profile/Posts/Posts";
+import {AppStateType} from "../../../redux/redux-store";
+import {FollowAC, SetUsersAC, UnfollowAC, UserType} from "../../../redux/users-page-reducer";
+import {Dispatch} from "redux";
 
-
-
-const mapStateToProps = () => {
-
+type mapStateToPropsType = {
+    users: UserType[]
 }
 
-const mapDispatchToProps = () => {
+type mapDispatchToPropsType = {
 
+    followAC: (id: number) => void
+    unFollowAC :(id: number) => void
+    setUsersAC:(newUsers: UserType[]) => void
+}
+
+const mapStateToProps = (state: AppStateType):mapStateToPropsType => {
+    return {
+        users: state.users
+    }
+}
+
+const mapDispatchToProps = (dispatch:Dispatch):mapDispatchToPropsType => {
+    return {
+        followAC: (id: number)=> {dispatch(FollowAC(id))},
+        unFollowAC: (id: number)=> {dispatch(UnfollowAC(id))},
+        setUsersAC: (newUsers: UserType[])=> {dispatch(SetUsersAC(newUsers))},
+    }
 }
 
 const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Posts)
