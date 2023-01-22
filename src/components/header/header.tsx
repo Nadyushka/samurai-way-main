607298b4b4968987d81fef500a9d955f2173ce73
Nadyushka <12nadya11@gmail.com>
2023-01-22 15:48:15 +0300

    lesson 61 samurai-way add authReducer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import s from './header.module.css'
+import {NavLink} from "react-router-dom";
+import {initialStateType} from "../../redux/auth-reducer";
+import { HeaderContainerType } from './headerContainer';
 
-const Header = () => {
+type propsType = HeaderContainerType
+
+const Header = (props: propsType) => {
     return (
         <div className={s.headerContainer}>
             <img
                 src='https://upload.wikimedia.org/wikipedia/ru/thumb/1/18/FC_Wolverhampton_Wanderers_Logo.svg/1182px-FC_Wolverhampton_Wanderers_Logo.svg.png'/>
+            <div className={s.header_loginBlock}>
+                {props.isFetching ?
+                   props.data.login :
+                    <NavLink to={'/login'}>
+                        Login
+                    </NavLink>}
+
+            </div>
         </div>
     );
 };
--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,68 +1,52 @@
-import {dispatchTypes} from "./state";
-
-const ADD_NEW_MESSAGE = 'ADD_NEW_MESSAGE'
-const CHANGE_NEW_MESSAGE = 'CHANGE_NEW_MESSAGE'
-
-export const addNewMessageActionCreator = (newMessage: string) => ({
-    type: ADD_NEW_MESSAGE,
-    newMessageText: newMessage
-}) as const
-
-export const changeNewMessageActionCreator = (newMessageText: string) => ({
-    type: CHANGE_NEW_MESSAGE,
-    newMessageText: newMessageText
-}) as const
-
-export type changeNewMessageActionCreatorType = ReturnType<typeof changeNewMessageActionCreator>
-export type addNewMessageActionCreatorType = ReturnType<typeof addNewMessageActionCreator>
+const SET_USER_DATA = 'SET_USER_DATA'
 
 
-export type contactDataType = {
-    id: number
-    name: string
-}
-
-export type messageDataType = {
-    id: number
-    message: string
-}
-
-let initialState = {
-    contacts: [
-        {id: 1, name: 'Bison'},
-        {id: 2, name: 'John'},
-        {id: 3, name: 'Alexa'},
-        {id: 4, name: 'Peter'},
-        {id: 5, name: 'Sandra'}] as contactDataType[]
-    ,
-    dialogs: [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How are you?'},
-        {id: 3, message: 'Enjoy your day'}
-    ] as messageDataType[],
-    newMessageText: '',
-}
-
-type initialStateType = typeof initialState
-
-export const messagePageReducer = (state: initialStateType = initialState, action: dispatchTypes): initialStateType => {
-    if (action.type === ADD_NEW_MESSAGE) {
-        let newMessage: messageDataType = {
-            id: new Date().getSeconds(),
-            message: action.newMessageText,
-        }
-        let newState = {...state, dialogs: [...state.dialogs,newMessage]}
-        newState.newMessageText = ''
-        return newState
-       // state.dialogs.push(newMessage)
-       // state.newMessageText = ''
-    } else if (action.type === CHANGE_NEW_MESSAGE) {
-       // state.newMessageText = action.newMessageText
-        return {...state,newMessageText: action.newMessageText }
-
+export const setUserDataAC = (id: number | null, email: string | null, login: string | null) => ({
+    type: SET_USER_DATA,
+    data: {
+        id,
+        email,
+        login,
     }
+}) as const
 
-    return state;
+
+export type setUserDataACType = ReturnType<typeof setUserDataAC>
+
+
+export type initialStateType = {
+    data: {
+        id: number | null
+        email: string | null
+        login: string | null
+    },
+    isFetching: boolean
+}
+
+let initialState: initialStateType = {
+    data: {
+        id: null,
+        email: null,
+        login: null,
+    },
+    isFetching: false,
+}
+
+
+export const authReducer = (state: initialStateType = initialState, action: setUserDataACType): initialStateType => {
+    switch (action.type) {
+        case SET_USER_DATA:
+            return {
+                ...state,
+                data: {
+                    ...state.data,
+                    id: action.data.id, email: action.data.email, login: action.data.login
+                },
+                isFetching: true,
+            }
+        default:
+            return state;
+    }
 
 }
 
