08bf69c04144feadbebb6b851c6e0f4fb678aa91
Nadyushka <12nadya11@gmail.com>
2023-02-06 20:22:05 +0300

    lesson 75 samurai-way start

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -5,6 +5,20 @@ const Login = () => {
     return (
         <div>
             <h1>Login</h1>
+            <form>
+                <div>
+                    <input placeholder={'Login'}/>
+                </div>
+                <div>
+                    <input type={'password'}  placeholder={'Password'}/>
+                </div>
+                <div>
+                    <input type={'checkbox'} /> Remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
         </div>
     );
 };
