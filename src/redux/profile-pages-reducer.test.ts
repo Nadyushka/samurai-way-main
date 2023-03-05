da305b1d1f040bafc566b54187774f0fa2130a45
Nadyushka <12nadya11@gmail.com>
2023-03-05 18:14:41 +0300

    lesson 89 samurai-way finish

--- src/redux/profile-pages-reducer.test.ts
+++ src/redux/profile-pages-reducer.test.ts
@@ -18,3 +18,14 @@ test('the message should be added', ()=> {
 
 
 
+test('new message should be \'I am so happy\'', ()=> {
+    let action = addNewPostActionCreator('I am so happy')
+    let newState = profilePageReducer(initialState,action)
+    expect(newState.posts[2].post).toBe('I am so happy')
+})
+
+test('The message should be deleted', ()=> {
+    let action = postDeleteActionCreator(1)
+    let newState = profilePageReducer(initialState,action)
+    expect(newState.posts.length).toBe(1)
+})
