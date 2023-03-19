7b4322b84a4bdc5033078fb1df9543925b1dd3e8
Nadyushka <12nadya11@gmail.com>
2023-03-19 18:16:05 +0300

    structure code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './App.module.css';
-import Aside from "./components/aside/aside";
-import HeaderContainer from "./components/header/headerContainer";
+import Aside from "./components/c2-aside/aside";
+import HeaderContainer from "./components/c1-header/headerContainer";
 import Main from "./components/main/main";
 
 
