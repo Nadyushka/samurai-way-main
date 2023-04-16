fd56d3f2b67cd9684ac8df27537d0d6d03dc246c
Nadyushka <12nadya11@gmail.com>
2023-04-16 14:45:54 +0300

    add style to aside

--- src/s3-UI/components/c1-features/f2-aside/aside.tsx
+++ src/s3-UI/components/c1-features/f2-aside/aside.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import s from './aside.module.css';
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation} from "react-router-dom";
 
 
 const Aside = () => {
+
+    const location = useLocation();
+    const { pathname } = location;
+    const splitLocation = pathname.split("/");
+
     return (
         <div className={s.aside}>
-            <div><NavLink to='/f3-main/profile' > Profile </NavLink></div>
-            <div><NavLink to='/f3-main/messages'> Messages </NavLink></div>
-            <div>News</div>
-            <div>Music</div>
+            <div><NavLink to='/f3-main/profile' activeClassName={s.activeLink} className={splitLocation[1] === "f3-main/profile" ? "s.activeLink" : ""}> Profile </NavLink></div>
+            <div><NavLink to='/f3-main/messages' activeClassName={s.activeLink} className={splitLocation[1] === "f3-main/messages" ? "s.activeLink" : ""}> Messages </NavLink></div>
+            <div><NavLink to={''}>News</NavLink></div>
+            <div><NavLink to={''}>Music</NavLink></div>
             <br/>
             <br/>
-            <div><NavLink to='/f3-main/users' > Users </NavLink></div>
+            <div><NavLink to='/f3-main/users' activeClassName={s.activeLink} className={splitLocation[1] === "f3-main/users" ? "s.activeLink" : ""}> Users </NavLink></div>
             <br/>
-            <div>Settings</div>
+            <div><NavLink to={''}>Settings</NavLink></div>
         </div>
     );
 };
