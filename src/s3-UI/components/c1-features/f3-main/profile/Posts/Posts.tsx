fabf55f50f1b7eb66aece99475b2a66d7c2ab478
Nadyushka <12nadya11@gmail.com>
2023-04-17 21:57:51 +0300

    feat: finished add comments with Local Storage

--- src/s3-UI/components/c1-features/f3-main/profile/Posts/Posts.tsx
+++ src/s3-UI/components/c1-features/f3-main/profile/Posts/Posts.tsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from "./Posts.module.css"
 import {PostsPropsType} from "./PostsContainer";
 import deleteIcon from '../../../../../../s4-common/assets/pictures/delete.svg'
+import {getPosts, postsDataType} from '../../../../../../s2-BLL/profile-pages-reducer';
+import {useDispatch} from "react-redux";
 
 
 const Posts = (props: PostsPropsType) => {
@@ -9,9 +11,12 @@ const Posts = (props: PostsPropsType) => {
     const [inputValue, setInputValue] = useState<string>('')
     const [error, setError] = useState<string>('')
 
+    const dispatch = useDispatch()
+
+
     const addNewPost = (value: string) => {
         if (value.length > 0) {
-            props.addNewPostActionCreator(inputValue)
+            props.addPostTC(inputValue)
             setInputValue('')
         } else {
             setError('Min length is 1 symbol')
@@ -21,14 +26,33 @@ const Posts = (props: PostsPropsType) => {
         }
     }
 
+    useEffect(() => {
+        if (localStorage.getItem('firstOpen') === 'no') {
+            dispatch(getPosts(JSON.parse(localStorage.getItem('postsLocalStorage')!)))
+        } else {
+            localStorage.setItem('firstOpen', 'no')
+            let defaultValue = [
+                {id: 1, post: 'Hello, everyone', likesCount: 10, commentsCount: 0},
+                {id: 2, post: 'I am happy', likesCount: 13, commentsCount: 0}
+            ]
+            localStorage.setItem('postsLocalStorage', JSON.stringify(defaultValue))
+            dispatch(getPosts(defaultValue))
+        }
+    }, [])
+
+    const deletePostHandler = (postId: number) => {
+       props.deletePostTC(postId)
+    }
+
     return (
         <div className={s.posts}>
 
-            <input className={s.postsInput} value={inputValue} onChange={(e) => {
-                setInputValue(e.currentTarget.value)
-                setError('')
-            }}/>
-            <button  disabled={error.length > 0} onClick={() => addNewPost(inputValue)}>Submit</button>
+            <input className={s.postsInput} value={inputValue} placeholder={'Share your thoughts'}
+                   onChange={(e) => {
+                       setInputValue(e.currentTarget.value)
+                       setError('')
+                   }}/>
+            <button disabled={error.length > 0} onClick={() => addNewPost(inputValue)}>Submit</button>
             <div className={error ? `${s.errorInfo} ${s.errorInfo_active}` : `${s.errorInfo} `}>{error}</div>
 
             <div className={s.post}>
@@ -36,7 +60,7 @@ const Posts = (props: PostsPropsType) => {
                     return <div className={s.postItem} key={p.id}>
                         <div className={s.postText}>
                             <span>{p.post}</span>
-                            <img src={deleteIcon}/>
+                            <img src={deleteIcon} onClick={() => deletePostHandler(p.id)}/>
                         </div>
                         <div className={s.postReaction}>
                             <div className={s.postLikes}>likes: {p.likesCount}</div>
