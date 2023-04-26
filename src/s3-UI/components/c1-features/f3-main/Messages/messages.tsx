ee26c97cf14184b65e1aac38a4caf634ebcd36e2
Nadyushka <12nadya11@gmail.com>
2023-04-26 20:14:59 +0300

    finish message page

--- src/s3-UI/components/c1-features/f3-main/Messages/messages.tsx
+++ src/s3-UI/components/c1-features/f3-main/Messages/messages.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import s from "./messages.module.css"
 import {DialogsContainer} from "./Dialogs/DialogsContainer";
-import ContactsContainer from "./Contacts/ContactsContainer";
-
 
 const Messages = () => {
-
 
     return (
         <div className={s.messages}>
