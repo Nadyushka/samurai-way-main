b67cece72763523fd836f629531762f11e1899c4
Nadyushka <12nadya11@gmail.com>
2023-05-12 21:05:29 +0300

    fix start with login

--- src/s3-UI/components/c1-features/f3-main/main.tsx
+++ src/s3-UI/components/c1-features/f3-main/main.tsx
@@ -29,12 +29,12 @@ class Main extends React.Component<MainContainerType, any> {
 
     render() {
 
-       // if (!this.props.initialized) {return <Preloader/>}
+       if (!this.props.initialized) {return <Preloader/>}
 
         return (
             <div className={s.main}>
                 <Switch>
-                    <Route exact={true} path="/" render={() => <LoginContainer/>}/>
+                    <Route path="/" render={() => <LoginContainer/>}/>
                     <Route path="/f3-main/profile/:userId?" render={() => {
                         return (
                             <Suspense fallback={<div><Preloader/></div>}>
