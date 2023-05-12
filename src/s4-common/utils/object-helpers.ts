c69238d36cb546392204fdfe5a7b16f0887f53ba
Nadyushka <12nadya11@gmail.com>
2023-05-12 22:20:35 +0300

    Updates

--- src/s4-common/utils/object-helpers.ts
+++
@@ -1,8 +0,0 @@
-export const updateObjectInArray = (items:Array<any>, objPropName: string, itemsId: number, newObjProps: {}) => {
-    return items.map(u => {
-        if (u[objPropName] === itemsId) {
-            return {...u, ...newObjProps}
-        }
-        return u
-    })
-}
--- src/s4-common/utils/validators.ts
+++
@@ -1,9 +0,0 @@
-export const required = (value: any) => {
-    if (value) return undefined
-    return 'Field is required'
-}
-
-export const maxLengthCreator = (maxLength: number) => (value: any) => {
-    if(value.length > maxLength) return `Max length is ${maxLength}`
-    return undefined
-}
