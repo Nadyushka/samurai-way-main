a472fff2be4f2cba77a67129115c01c86de3e128
Nadyushka <12nadya11@gmail.com>
2023-05-12 22:19:27 +0300

    fix mistake

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import App from "./App";
 import React from "react";
 import './index.css';
 import {store} from './s2-BLL/redux-store'
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root')
 );
 
